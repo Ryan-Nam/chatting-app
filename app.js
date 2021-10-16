115ad7ca55b796496cd2a7601204868e1c90124d
Ryan-Nam <alfofh7060@gmail.com>
2021-10-17 00:07:36 +1000

    Add server

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const http = require("http")
 const app = express();
 const path = require("path");
+// express 로 구현한 app을 담아서, express 가 https 를 담아서 실행
+const server = http.createServer(app);
+
+const socketIO = require("socket.io")
+const io = socketIO(server);
 
 //console.log(__dirname)
 // __dirname -> project folder path
@@ -9,6 +15,15 @@ const path = require("path");
 //서버가 실행되면 실행될 파일 지정
 app.use(express.static(path.join(__dirname, "src")));
 
+
+io.on("connection", (socket) => {
+  // console.log("Connection is success")
+  socket.on("chatting", (data)=> {
+    console.log(data);
+    io.emit("chatting", `Hi, I am server, nice to meet you ${data} `)
+  })
+})
+
 // 서버 실행을 위해 port 필요
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`Server is running ${PORT}`))
+server.listen(PORT, ()=> console.log(`Server is running ${PORT}`))
