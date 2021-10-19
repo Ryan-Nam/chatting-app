b7920775a8bcb5ac181cad128bb7b0e6da8d6a60
Ryan-Nam <alfofh7060@gmail.com>
2021-10-19 22:50:50 +1000

    Add All

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,6 +1,11 @@
 "use strict"
 const socket = io();
 
+const nickname = document.querySelector("#nickname");
+const chatList = document.querySelector(".chatting-list");
+const chatInput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector(".send-button");
+
 
 socket.emit("chatting", "from front");
 
