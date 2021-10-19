bf899bb6dffb28ad5726ea5e8825c692c7deb79a
Ryan-Nam <alfofh7060@gmail.com>
2021-10-19 23:01:43 +1000

    Add transfer data to server

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,13 +1,24 @@
 "use strict"
 const socket = io();
 
+
+//DOM
 const nickname = document.querySelector("#nickname");
 const chatList = document.querySelector(".chatting-list");
 const chatInput = document.querySelector(".chatting-input");
 const sendButton = document.querySelector(".send-button");
 
+// Event 
+sendButton.addEventListener("click", ()=>{
+  // make it as object
+  const param = {
+    name: nickname.value,
+    msg: chatInput.value
+  }
+  socket.emit("chatting", param);
+})
 
-socket.emit("chatting", "from front");
+
 
 socket.on("chatting", (data)=>{
   console.log(data);
